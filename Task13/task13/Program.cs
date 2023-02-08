1f7115c282ea334087f7b2347fc2596f4434cb2e
anonimvt <75793665+anonimvt@users.noreply.github.com>
2023-02-08 10:31:56 +0400

    Task13 Second Commit

---
+++ Task13/task13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace NewProgram;
+
+public class Program
+{
+    public static void Main()
+    {   
+        Console.WriteLine("Введите число");
+        int n = int.Parse(Console.ReadLine());
+        int k = (int)Math.Log10(n)-2;
+        Console.WriteLine(k < 0 ? "Нет третьей цифра" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+        Console.ReadLine();
+    }
+}
